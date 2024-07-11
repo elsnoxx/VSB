c792686f3f56460e271d8d5a95b0a6695a9779b7
elsnoxx <ficek.risa@gmail.com>
2024-07-11 07:36:27 +0200

    APPS - pridani ukolu

--- 2_semestr/APPS/cuda3/main6_unm_ani.cpp
+++ 2_semestr/APPS/cuda3/main6_unm_ani.cpp
@@ -124,6 +124,63 @@ void animate_rotation(cv::Mat &background, cv::Mat &orig_img, float start_angle,
     cv::waitKey(0); // Wait indefinitely until a key is pressed
 }
 
+void animate_rotation_task(cv::Mat &background, cv::Mat &orig_img, cv::Mat &orig_img2, float start_angle, float end_angle, float step, int pos_x, int pos_y)
+{
+    CudaImg cuda_background, cuda_orig_img, cuda_orig_img3, cuda_rotate_img, cuda_rotate_img2, cuda_rotate_img3, cuda_result;
+    cv::Mat rotate_img(orig_img.size(), orig_img.type());
+    cv::Mat rotate_img2(orig_img2.size(), orig_img2.type());
+    set_cuda_img(background, cuda_background);
+    set_cuda_img(orig_img, cuda_orig_img);
+    set_cuda_img(rotate_img, cuda_rotate_img);
+    set_cuda_img(orig_img2, cuda_orig_img3);
+    set_cuda_img(rotate_img2, cuda_rotate_img2);
+    
+
+    cv::Mat result = background.clone();
+    set_cuda_img(result, cuda_result);
+
+    // Initialize video writer
+    //cv::VideoWriter l_video("rotation.mkv", cv::VideoWriter::fourcc('H', '2', '6', '4'), 25, background.size());
+
+    for (float angle = start_angle; angle <= end_angle; angle += step)
+    {
+        result = background.clone(); // Reset the result image to the original background
+        set_cuda_img(result, cuda_result);
+
+        // Rotate the first image at the given angle
+        //cu_run_rotate(cuda_orig_img, cuda_rotate_img, angle);
+        //cu_insertimage(cuda_result, cuda_rotate_img, pos_x, pos_y, 255);
+
+        // Rotate the second image three times faster
+        
+        cu_insertimage(cuda_result, cuda_orig_img, (background.cols / 2) - orig_img.cols / 2, (background.rows / 2) - orig_img.rows / 2, 255);
+
+        float t_sin = sinf(angle);
+        float t_cos = cosf(angle);
+        printf("sin %f, cos %f\n", t_sin, t_cos);
+        //cu_run_rotate(cuda_orig_img, cuda_rotate_img2, 90 );
+        cu_run_rotate(cuda_orig_img3, cuda_rotate_img2, angle * 2);
+        //cu_insertimage(cuda_result, cuda_rotate_img2, t_sin * orig_img.cols + pos_x + 400, t_cos * orig_img.cols + pos_y + 200, 255);
+        cu_insertimage(cuda_result, cuda_rotate_img2, t_sin * orig_img.cols + (background.cols / 2) - orig_img.cols / 2, t_cos * orig_img.cols + (background.rows / 2) - orig_img.rows / 2, 255);
+        // Rotate the third image at the same cuda_rotate_img3 as the first one
+        //cu_run_rotate(cuda_rotate_img, cuda_rotate_img3, angle);
+        //cu_insertimage(cuda_result, cuda_rotate_img3, pos_x + 700, pos_y + 400, 255);
+
+        // Display the result
+        cv::imshow("Rotation Animation", result);
+        
+        // Write the frame to the video
+        //l_video.write(result);
+
+        cv::waitKey(50); // Adjust the delay to control the speed of the animation
+    }
+
+    // Release the video writer
+    //l_video.release();
+
+    cv::waitKey(0); // Wait indefinitely until a key is pressed
+}
+
 
 void animate_watchtig(cv::Mat &background, std::vector<cv::Mat> &tiger_imgs, int fps, int speed)
 {
@@ -275,6 +332,25 @@ int main(int argc, char *argv[])
 
         animate_watchtig(background, tiger_imgs, 15, 15);
 
+        break;
+    }
+    case 4:
+    {
+        cv::Mat background = cv::imread("img/windmill/louka.jpg", cv::IMREAD_UNCHANGED);
+        cv::Mat orig_img = cv::imread("img/windmill/windmill.png", cv::IMREAD_UNCHANGED);
+        cv::Mat orig_img2 = cv::imread("img/windmill/windmill.png", cv::IMREAD_UNCHANGED);
+
+        if (orig_img.empty())
+        {
+            std::cerr << "Image could not be loaded!" << std::endl;
+            return -1;
+        }
+
+        // Zvolené pozice pro vložení otáčejícího se obrázku
+        int pos_x = 10;
+        int pos_y = 10;
+
+        animate_rotation_task(background, orig_img, orig_img2, 0, 100 * CV_PI, 0.1, pos_x, pos_y);
         break;
     }
     default:
