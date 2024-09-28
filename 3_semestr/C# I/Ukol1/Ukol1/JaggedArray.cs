2c358eb42bbb790c7829eb78f40e54e038647cc5
Richard Ficek <richard.ficek@hyundai-autoever.cz>
2024-09-28 13:04:01 +0200

    c# I ukol 1 done

---
+++ 3_semestr/C# I/Ukol1/Ukol1/JaggedArray.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ukol1
+{
+    internal class JaggedArray
+    {
+        bool[][] jaggedArray = new bool[40][];
+
+        public void Run()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < jaggedArray.GetLength(0); i++)
+            {
+                jaggedArray[i] = new bool[40];
+                for (int j = 0; j < jaggedArray.Length ; j++)
+                {
+                    jaggedArray[i][j] = false;
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                int x = rnd.Next(40);
+                int y = rnd.Next(40);
+                CreateZBlock(x, y, jaggedArray);
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                int x = rnd.Next(40);
+                int y = rnd.Next(40);
+                CreateTBlock(x, y, jaggedArray);
+            }
+
+
+            MainLoop(jaggedArray);
+        }
+        public void MainLoop(bool[][] jaggedArray)
+        {
+            int time_sleep = 1000;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            while (true)
+            {
+                Console.SetCursorPosition(0, 0);
+                Print();
+                for (int i = jaggedArray.GetLength(0) - 1; i >= 0; i--)
+                {
+                    for (int j = 0; j < jaggedArray.Length; j++)
+                    {
+                        if (jaggedArray[i][j])
+                        {
+                            if (i == jaggedArray.GetLength(0) - 1)
+                            {
+                                jaggedArray[i][j] = false;
+                            }
+                            else
+                            {
+                                // Posun hodnoty dolů
+                                jaggedArray[i + 1][ j] = jaggedArray[i][j];
+                                jaggedArray[i][j] = false; // Vymazání původního místa
+                            }
+                        }
+                    }
+                }
+                for (int j = 0; j < jaggedArray.Length; j++)
+                {
+                    Console.Write('-');
+                    Console.Write('-');
+                }
+                Thread.Sleep(time_sleep - 5);
+            }
+        }
+
+        public void CreateTBlock(int x, int y, bool[][] jaggedArray)
+        {
+            // Zkontrolujeme, zda kostička nepřesáhne hranice pole
+            if (x >= 0 && x + 1 < jaggedArray.GetLength(0) && y >= 0 && y + 2 < jaggedArray.Length)
+            {
+                // Tvar T (vodorovná část)
+                jaggedArray[x][y] = true;
+                jaggedArray[x][y + 1] = true;
+                jaggedArray[x][y + 2] = true;
+
+                // Tvar T (svislá část)
+                jaggedArray[x + 1][y + 1] = true;
+                jaggedArray[x + 2][y + 1] = true;
+            }
+        }
+
+        public void CreateZBlock(int x, int y, bool[][] jaggedArray)
+        {
+            // Zkontrolujeme, zda kostička nepřesáhne hranice pole
+            if (x >= 0 && x + 1 < jaggedArray.GetLength(0) && y >= 0 && y + 2 < jaggedArray.Length)
+            {
+                // Tvar Z (horní část)
+                jaggedArray[x][y] = true;
+                jaggedArray[x][y + 1] = true;
+
+                // Tvar Z (spodní část)
+                jaggedArray[x + 1][y + 1] = true;
+                jaggedArray[x + 1][y + 2] = true;
+            }
+        }
+        public void Print()
+        {
+
+            for (int i = 0; i < jaggedArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < jaggedArray.Length; j++)
+                {
+                    if (jaggedArray[i][j])
+                    {
+                        Console.Write('\u2588');
+                        Console.Write('\u2588');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                        Console.Write(' ');
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+
+}
---
+++ 3_semestr/C# I/Ukol1/Ukol1/StructureArray.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ukol1
+{
+    public struct Brick
+    {
+        public int x;
+        public int y;
+
+        public Brick(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public struct CompositeBrick
+    {
+        public int x;
+        public int y;
+        List<Brick> bricks;
+
+        public CompositeBrick(int x, int y, List<Brick> bricks)
+        {
+            this.x = x;
+            this.y = y;
+            this.bricks = bricks;
+        }
+
+        // Metoda pro posunutí složené kostičky dolů
+        public void MoveDown()
+        {
+            this.y++; // Posuneme složenou kostičku dolů
+        }
+
+        // Metoda pro získání absolutních souřadnic jednotlivých bricks
+        public List<Brick> GetAbsoluteBricks()
+        {
+            List<Brick> absoluteBricks = new List<Brick>();
+            foreach (var brick in bricks)
+            {
+                absoluteBricks.Add(new Brick(brick.x + x, brick.y + y));
+            }
+            return absoluteBricks;
+        }
+    }
+    internal class StructureArray
+    {
+        private List<CompositeBrick> compositeBricks = new List<CompositeBrick>(); // Seznam složených kostiček
+
+        private const int width = 40;  // Šířka herního pole
+        private const int height = 20; // Výška herního pole
+
+        // Metoda pro přidání nové kostičky do hry
+        public void AddCompositeBrick(CompositeBrick compositeBrick)
+        {
+            compositeBricks.Add(compositeBrick);
+        }
+
+        // Metoda pro vytvoření kostičky tvaru T
+        public CompositeBrick CreateTBrick(int x, int y)
+        {
+            List<Brick> bricks = new List<Brick>
+            {
+                new Brick(0, 0), // Levý díl T
+                new Brick(1, 0), // Střední díl T
+                new Brick(2, 0), // Pravý díl T
+                new Brick(1, 1)  // Střední dolní díl T
+            };
+            return new CompositeBrick(x, y, bricks);
+        }
+
+        // Metoda pro vytvoření kostičky tvaru Z
+        public CompositeBrick CreateZBrick(int x, int y)
+        {
+            List<Brick> bricks = new List<Brick>
+            {
+                new Brick(0, 0), // Levý horní díl Z
+                new Brick(1, 0), // Pravý horní díl Z
+                new Brick(1, 1), // Levý dolní díl Z
+                new Brick(2, 1)  // Pravý dolní díl Z
+            };
+            return new CompositeBrick(x, y, bricks);
+        }
+
+        // Metoda pro posun všech kostiček dolů
+        public void MoveAllBricksDown()
+        {
+            foreach (var compositeBrick in compositeBricks)
+            {
+                compositeBrick.MoveDown();
+            }
+        }
+
+        // Metoda pro vykreslení scény
+        public void PrintScene()
+        {
+            char[,] scene = new char[height, width];
+
+            // Inicializace prázdné scény
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    scene[i, j] = ' '; // Prázdné místo
+                }
+            }
+
+            // Přidání kostiček do scény
+            foreach (var compositeBrick in compositeBricks)
+            {
+                List<Brick> absoluteBricks = compositeBrick.GetAbsoluteBricks();
+                foreach (var brick in absoluteBricks)
+                {
+                    if (brick.y >= 0 && brick.y < height && brick.x >= 0 && brick.x < width)
+                    {
+                        scene[brick.y, brick.x] = '\u2588'; // Plný čtverec
+                    }
+                }
+            }
+
+            // Vykreslení scény do konzole
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(scene[i, j]);
+                    Console.Write(scene[i, j]); // Dvojité vykreslení pro lepší viditelnost čtverců
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // Hlavní smyčka hry
+        public void Run()
+        {
+            AddCompositeBrick(CreateTBrick(5, 0)); // Přidání kostičky T
+            AddCompositeBrick(CreateZBrick(10, 0)); // Přidání kostičky Z
+
+            while (true)
+            {
+                PrintScene();  // Vykreslení scény
+                MoveAllBricksDown();  // Posun všech kostiček dolů
+                Thread.Sleep(1000);  // Pauza na 1 sekundu
+            }
+        }
+    }
+}
