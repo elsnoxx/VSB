cfc0e1cbd0913568a3d42d9e71fde1effb8a40b6
Richard Ficek <richard.ficek@hyundai-autoever.cz>
2024-10-03 07:49:17 +0200

    Update Program.cs

--- 3_semestr/C# I/Ukoly/Ukol1/Program.cs
+++ 3_semestr/C# I/Ukoly/Ukol1/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics;
+using System.Globalization;
 using System.IO.Pipes;
+using System.Runtime.Intrinsics.X86;
 
 
 namespace Ukol1
@@ -11,10 +13,14 @@ namespace Ukol1
             Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("cs-CZ");
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            //string data = File.ReadAllText(args[0]);
-            string data = File.ReadAllText(@"C:\\Users\\admin\\Documents\\GitHub\\VSB\\3_semestr\\C# I\\Ukoly\\Ukol1\\data.txt");
+            string data = File.ReadAllText(args[0]);
+            //string data = File.ReadAllText(@"C:\\Users\\admin\\Documents\\GitHub\\VSB\\3_semestr\\C# I\\Ukoly\\Ukol1\\data.txt");
+
+
 
             Product[] products = ParseData(data);
+
+
 
             double sum = GetTotalProductsPrice(products);
             double averageWeight = GetAverageItemWeight(products);
@@ -24,11 +30,11 @@ namespace Ukol1
             {
                 if (produkt.Count == null)
                 {
-                    Console.WriteLine(produkt.Name + ": " + "neznámé množství; " + produkt.ItemPrice + " Kč " + produkt.Weight.GetNormalizedValue() + " kg");
+                    Console.WriteLine(produkt.Name + ": " + "neznámé množství; " + produkt.ItemPrice + " Kč");
                 }
                 else
                 {
-                    Console.WriteLine(produkt.Name + ": " + produkt.Count + " ks; " + produkt.ItemPrice + " Kč " + produkt.Weight.GetNormalizedValue() * produkt.Count.Value );
+                    Console.WriteLine(produkt.Name + ": " + produkt.Count + " ks; " + produkt.ItemPrice + " Kč");
                 }
 
             }
@@ -65,8 +71,6 @@ namespace Ukol1
             return Math.Round(totalWeight / totalCount, 3);
         }
 
-
-
         public static Product[] ParseData(string data)
         {
             List<Product> products = new List<Product>();
@@ -78,79 +82,67 @@ namespace Ukol1
             foreach (string line in lines) 
             {
                 string test = line.Trim();
-                Console.WriteLine(test);
-                if (test == "products:")
+                if (test == "products:" || string.IsNullOrEmpty(test))
                 {
-                    continue;  // Tento řádek přeskočí a pokračuje dalším řádkem
+                    continue;
                 }
 
-                if (!string.IsNullOrEmpty(test))
+
+                if (test.StartsWith("- price:"))
                 {
-                    if (test.StartsWith("- price:"))
+                    index = line.IndexOf("price: ");
+                    if (index != -1)
                     {
-                        index = line.IndexOf("price: ");
-                        if (index != -1)
+                        double price;
+                        if (double.TryParse(test.Substring(7).Split(":")[1].Trim().Replace(".", ","), out price))
                         {
-                            //Console.WriteLine(test.Substring(7).Split(":")[1].Trim());
-                            double price;
-                            if (double.TryParse(test.Substring(7).Split(":")[1].Trim().Replace(".", ","), out price))
+                            currentProduct.ItemPrice = price;
+                        }
+                    }
+                }
+
+                else if (test.StartsWith("- quantity:"))
+                {
+                    index = line.IndexOf("quantity:");
+                    if (index != -1)
+                    {
+                        int quantity;
+                        if (int.TryParse(test.Substring(10).Split(":")[1].Trim(), out quantity))
+                        {
+                            currentProduct.Count = quantity;
+                        }
+                    }
+
+                }
+
+                else if (test.StartsWith("- weight:"))
+                {
+                    index = line.IndexOf("weight:");
+                    if (index != -1)
+                    {
+                        string[] weightParts = test.Substring(7).Split(":")[1].Split(' ');
+                        double weightValue;
+                        if (double.TryParse(weightParts[1].Replace(".", ","),  out weightValue))
+                        {
+                            currentProduct.Weight = new Weight
                             {
-                                currentProduct.ItemPrice = price;
-                            }
+                                Hodnota = weightValue,
+                                Jednotka = weightParts[2].Trim() == "g" ? Jednotka.Gram : (weightParts[2].Trim() == "dkg" ? Jednotka.Dekagram: Jednotka.Kilogram)
+                            };
                         }
                     }
-
-                    else if (test.StartsWith("- quantity:"))
+                }
+                else
+                {
+                    if (currentProduct.Name != null)
                     {
-                        index = line.IndexOf("quantity:");
-                        if (index != -1)
-                        {
-                            //Console.WriteLine(test.Substring(10).Split(":")[1].Trim());
-                            int quantity;
-                            if (int.TryParse(test.Substring(10).Split(":")[1].Trim(), out quantity))
-                            {
-                                currentProduct.Count = quantity;
-                            }
-                        }
-
+                        products.Add(currentProduct);
                     }
-
-                    else if (test.StartsWith("- weight:"))
-                    {
-                        index = line.IndexOf("weight:");
-                        if (index != -1)
-                        {
-                            string[] weightParts = test.Substring(7).Split(":")[1].Split(' ');
-                            double weightValue;
-                            if (double.TryParse(weightParts[1].Replace(".", ","),  out weightValue))
-                            {
-                                currentProduct.Weight = new Weight
-                                {
-                                    Hodnota = weightValue,
-                                    Jednotka = weightParts[2].Trim() == "g" ? Jednotka.Gram : (weightParts[2].Trim() == "dkg" ? Jednotka.Dekagram: Jednotka.Kilogram)
-                                };
-                            }
-                            //Console.Write(test.Substring(7).Split(":")[1].Split(' ')[1].Trim());
-                            //Console.Write(' ');
-                            //Console.Write(test.Substring(7).Split(":")[1].Split(' ')[2].Trim());
-                            //Console.WriteLine();
-                        }
-                    }
-                    else
-                    {
-                        // Pokud se objeví nová položka, přidáme předchozí produkt do seznamu
-                        if (currentProduct.Name != null)
-                        {
-                            products.Add(currentProduct);
-                        }
-
-                        // Vytvoříme nový produkt pro aktuální záznam
-                        currentProduct = new Product();
-                        currentProduct.Name = test.Split("-")[1].Split(":")[0].Trim();
-                    }
+                    currentProduct = new Product();
+                    currentProduct.Name = test.Split("-")[1].Split(":")[0].Trim();
                 }
             }
-            // Přidání posledního produktu po skončení cyklu
+            
             if (currentProduct.Name != null)
             {
                 products.Add(currentProduct);
