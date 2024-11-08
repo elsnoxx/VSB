05a2c29cff706b89f71437ffac2c332cbfb419e1
Richard Ficek <richard.ficek@hyundai-autoever.cz>
2024-11-08 09:44:13 +0100

    Update Projekt.cpp

--- 3_semestr/ALG II/Projekt/Projekt/Projekt.cpp
+++ 3_semestr/ALG II/Projekt/Projekt/Projekt.cpp
@@ -1,32 +1,58 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cstdio>
-
+#include <map>
+#include <vector>
 
 using namespace std;
 
+struct Node {
+    string name;
+    vector<string> passage;
 
-int main()
-{
+    Node() : name(""), passage({}) {}
+    Node(string name, vector<string> passage) : name(name), passage(passage) {}
+};
+
+int main() {
+    // Otev?ení souboru
     ifstream file("C:\\Users\\admin\\Documents\\GitHub\\VSB\\3_semestr\\ALG II\\Projekt\\Data\\test0.txt");
+    if (!file) {
+        cerr << "Nepoda?ilo se otev?ít soubor." << endl;
+        return 1;
+    }
 
+    map<string, Node> jeskyne;
     string myText;
 
     while (getline(file, myText)) {
-        // Output the text from the file
-        cout << myText << "\n";
+        size_t positionOfDash = myText.find("-");
+
+        string jeskyn1 = myText.substr(0, positionOfDash);
+        string jeskyn2 = myText.substr(positionOfDash + 1);
+
+        cout << jeskyn1 << " " << jeskyn2 << endl;
+
+        
+        jeskyne[jeskyn1].name = jeskyn1;
+        jeskyne[jeskyn1].passage.push_back(jeskyn2);
+
+        jeskyne[jeskyn2].name = jeskyn2;
+        jeskyne[jeskyn2].passage.push_back(jeskyn1);
     }
 
-    int age;
 
-    cout << "Enter your age: ";
+    for (const auto& pair : jeskyne) {
+        const string& key = pair.first;
+        const Node& node = pair.second;
 
-    // get age from user
-    scanf_s("%d", &age);
+        cout << "Jeskyne " << key << " ma spojeni s: ";
+        for (const string& neighbor : node.passage) {
+            cout << neighbor << " ";
+        }
+        cout << endl;
+    }
 
-    // print age
-    cout << "Age = " << age;
 
     return 0;
 }
