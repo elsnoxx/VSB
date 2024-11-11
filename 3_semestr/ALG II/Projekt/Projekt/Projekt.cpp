a5dfd7ff963451e980329de2dab4f1ae4ba763ba
Richard Ficek <richard.ficek@hyundai-autoever.cz>
2024-11-11 13:39:18 +0100

    Update Projekt.cpp

--- 3_semestr/ALG II/Projekt/Projekt/Projekt.cpp
+++ 3_semestr/ALG II/Projekt/Projekt/Projekt.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include <string>
 #include <map>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,48 +15,68 @@ struct Node {
     Node(string name, vector<string> passage) : name(name), passage(passage) {}
 };
 
+void dfs(const string& currentCave, vector<string>& path, map<string, Node>& jeskyne, vector<vector<string>>& allPaths) {
+    path.push_back(currentCave);
+
+    if (currentCave == "end") {
+        allPaths.push_back(path);
+    }
+    else {
+        for (const string& neighbor : jeskyne[currentCave].passage) {
+            if (islower(neighbor[0]) && find(path.begin(), path.end(), neighbor) != path.end()) {
+                continue;
+            }
+            dfs(neighbor, path, jeskyne, allPaths);
+        }
+    }
+
+    path.pop_back();
+}
+
 int main() {
-    // Otev?en� souboru
-    ifstream file("..\\Data\\test0.txt");
-    if (!file) {
-        cerr << "Nepoda?ilo se otev?�t soubor." << endl;
-        return 1;
-    }
+    vector<string> FilesName = { "..\\Data\\test0.txt", "..\\Data\\test1.txt", "..\\Data\\test2.txt", "..\\Data\\test3.txt", "..\\Data\\trivial_test.txt"};
 
-    map<string, Node> jeskyne;
-    string myText;
+    for (int i = 0; i < FilesName.size(); i++) {
+        ifstream file(FilesName[i]);
+        if (!file) {
+            cerr << "Nepodařilo se otevřít soubor: " << FilesName[i] << endl;
+            return 1;
+        }
 
-    while (getline(file, myText)) {
-        size_t positionOfDash = myText.find("-");
+        map<string, Node> jeskyne;
+        string myText;
 
-        string jeskyn1 = myText.substr(0, positionOfDash);
-        string jeskyn2 = myText.substr(positionOfDash + 1);
+        while (getline(file, myText)) {
+            size_t positionOfDash = myText.find("-");
 
-        cout << jeskyn1 << " " << jeskyn2 << endl;
+            string jeskyn1 = myText.substr(0, positionOfDash);
+            string jeskyn2 = myText.substr(positionOfDash + 1);
 
-        
-        jeskyne[jeskyn1].name = jeskyn1;
-        jeskyne[jeskyn1].passage.push_back(jeskyn2);
+            jeskyne[jeskyn1].name = jeskyn1;
+            jeskyne[jeskyn1].passage.push_back(jeskyn2);
 
-        jeskyne[jeskyn2].name = jeskyn2;
-        jeskyne[jeskyn2].passage.push_back(jeskyn1);
-    }
+            jeskyne[jeskyn2].name = jeskyn2;
+            jeskyne[jeskyn2].passage.push_back(jeskyn1);
+        }
 
 
-    for (const auto& pair : jeskyne) {
-        const string& key = pair.first;
-        const Node& node = pair.second;
+        vector<vector<string>> allPaths;
+        vector<string> path;
+        dfs("start", path, jeskyne, allPaths);
 
-        cout << "Jeskyne " << key << " ma spojeni s: ";
-        for (const string& neighbor : node.passage) {
-            cout << neighbor << " ";
+        cout << FilesName[i] << ": " << allPaths.size() << endl;
+
+        /*
+        for (const auto& p : allPaths) {
+            for (const auto& cave : p) {
+                cout << cave << " ";
+            }
+            cout << endl;
         }
         cout << endl;
+        */
     }
-
-    int x;
-    cin >> x; 
-    cout << "Your number is: " << x;
 
     return 0;
 }
+
