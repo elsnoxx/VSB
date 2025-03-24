33592dfc25f3b39af2445cc64c50f085333f3b12
Richard Ficek <richard.ficek@hyundai-autoever.cz>
2025-03-24 13:54:11 +0100

    Update ukol1.py

--- 4_semestr/URO/cviko2/ukol1.py
+++ 4_semestr/URO/cviko2/ukol1.py
@@ -3,6 +3,7 @@ import random
 import hashlib
 
 abeceda = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 def vzorka():
     vysledok = list()
     for _ in range(0, 1000000):
@@ -12,17 +13,30 @@ def vzorka():
         vysledok.append(retazec)
     return vysledok
 
-def hash_it(text):
-    h = hashlib.md5()
+def hash_it(text, algorithm='md5'):
+    if algorithm == 'md5':
+        h = hashlib.md5()
+    elif algorithm == 'sha1':
+        h = hashlib.sha1()
+    elif algorithm == 'sha256':
+        h = hashlib.sha256()
+    else:
+        raise ValueError("Unsupported hashing algorithm")
+    
     data = bytes(text, encoding='utf8')
     h.update(data)  # mixer
     return h.hexdigest()
 
+def benchmark(algorithm):
+    t1 = time.time()
+    vygenerovane_retazce = vzorka()
+    for i in vygenerovane_retazce:
+        hash_it(i, algorithm)
+    t2 = time.time()
+    print(f'{algorithm.upper()} - Start Time: {t1}, End Time: {t2}')
+    print(f'{algorithm.upper()} - VYSLEDNY CAS V SEC: {t2-t1}')
 
-t1 = time.time()
-vygenerovane_retazce = vzorka()
-for i in vygenerovane_retazce:
-    hash_it(i)
-t2 = time.time()
-print(t1,'\t\t', t2)
-print('VYSLEDNY CAS V SEC: ', t2-t1)
+# Benchmarking different algorithms
+benchmark('md5')
+benchmark('sha1')
+benchmark('sha256')
