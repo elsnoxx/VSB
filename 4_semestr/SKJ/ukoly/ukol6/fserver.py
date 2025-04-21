25580f803b17bb1eee5c0c25848b1c188f02f963
elsnoxx <ficek.risa@gmail.com>
2025-04-21 19:26:10 +0200

    ukol done

--- 4_semestr/SKJ/ukoly/ukol6/fserver.py
+++ 4_semestr/SKJ/ukoly/ukol6/fserver.py
@@ -10,12 +10,16 @@ class Forecast(object):
         """
         Konstuktor predpovedi. Instancni promenne reprezentuji predana data.
         """
+        self.description = description
+        self.wind_force = wind_force
+        self.temperature = temperature
         pass
 
     def get_list(self):
         """
         Vraci trojici reprezentujici predpoved.
         """
+        return (self.description, self.wind_force, self.temperature)
         pass
 
 class ForecastCalendar(object):
@@ -30,6 +34,8 @@ class ForecastCalendar(object):
         """
         Konstruktor kalendare predpovedi. Instancni promenne reprezentuji predana data.
         """
+        self.password = password
+        self.forecasts = initial_values
         pass
 
     def get_forecast(self, date):
@@ -37,7 +43,12 @@ class ForecastCalendar(object):
         Vrati predpoved pro zadane datum jako retezec. V pripade, ze pro dane
         datum neexistuje predpoved. Vrati se retezec "No focecast".
         """
-        return "No forecast"
+        if date in self.forecasts:
+            forecast = self.forecasts[date]
+            data = forecast.get_list()
+            return f"Forecast for {date}: {data[0]} {str(data[1])} {str(data[2])}"
+        else:
+            return "No forecast"
 
     def update_forecast(self, password, date, description, wind_force, temperature):
         """
@@ -46,13 +57,25 @@ class ForecastCalendar(object):
         aktualizovat predpoved. v takovm priapde metoda vrati retezec "No
         update". Metoda muze aktualizovat stavajici predpoved nebo pridat novou.
         """
-        return "No update"
+        if password == self.password:
+            if date in self.forecasts:
+                self.forecasts[date] = Forecast(description, wind_force, temperature)
+            else:
+                self.forecasts[date] = Forecast(description, wind_force, temperature)
+            return f"Update: OK => {date} {description} {str(wind_force)} {str(temperature)}"
+        else:
+            return "Wrong password"
         
 def main():
 
     # TODO Pridat do initial_state data predpovedi tak, aby je mohl klient
     # precist.
-    initial_state = {}
+    initial_state = {
+        "2012-11-05": Forecast("sunny", 2.0, 22.5),
+        "2012-11-06": Forecast("partly cloudy", 3.5, 19.0),
+        "2012-11-07": Forecast("cloudy", 4.0, 15.5), 
+        "2012-11-08": Forecast("light rain", 5.5, 12.0)
+    }
 
     fcalendar = ForecastCalendar(initial_state, password = "master-of-weather")
 
