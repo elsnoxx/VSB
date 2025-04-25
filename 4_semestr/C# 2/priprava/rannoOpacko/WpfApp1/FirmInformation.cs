66ae4ea7e6f566a62550b608997f471fb8c162f0
elsnoxx <83233897+elsnoxx@users.noreply.github.com>
2025-04-25 06:16:26 +0200

    ranni opacko

---
+++ 4_semestr/C# 2/priprava/rannoOpacko/WpfApp1/FirmInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class FirmInformation
+    {
+        public string obchodniJmeno { get; set; }
+        public string dic { get; set; }
+        public string nazevObce { get; set; }
+
+    }
+}
---
+++ 4_semestr/C# 2/priprava/rannoOpacko/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Text.Json;
+using System.Windows.Markup;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Handle text changed event
+            if (TextBox1.Text.Length > 0)
+            {
+                GetIco.IsEnabled = true;
+            }
+            else
+            {
+                GetIco.IsEnabled = false;
+            }
+        }
+
+        private async void GetIco_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+            string ico = TextBox1.Text;
+            JsonData info = await getInfo(ico);
+            Window1 window1 = new Window1();
+            window1.DataContext = info;
+            window1.Show();
+
+        }
+
+        public async Task<JsonData> getInfo(string ico)
+        {
+            try
+            {
+                Uri url = new Uri("https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/" + ico);
+                HttpClient client = new HttpClient();
+                var response = client.GetAsync(url);
+
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    var options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+
+                    string jsonResponse = await response.Result.Content.ReadAsStringAsync();
+
+                    JsonData jsonData = JsonSerializer.Deserialize<JsonData>(jsonResponse, options);
+                    return jsonData;
+                }
+                else
+                {
+                    MessageBox.Show("Error: " + response.Result.StatusCode.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
