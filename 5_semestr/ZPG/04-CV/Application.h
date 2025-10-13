ed22bfeb3796017227318c15791fc2e8dc5ef2f3
Richard Ficek <ficek.risa@gmail.com>
2025-10-13 13:21:45 +0200

    cv4 created

---
+++ 5_semestr/ZPG/04-CV/Application.h
@@ -0,0 +1,33 @@
+#pragma once
+//Include GLEW
+#include <GL/glew.h>
+//Include GLFW
+#include <GLFW/glfw3.h> 
+
+//Include the standard C++ headers  
+#include <stdlib.h>
+#include <stdio.h>
+
+class Application {
+	public:
+
+		void initialization();
+		void createShaders();
+		void createModels();
+
+		void printVersionInfo();
+		void updateViewport();
+
+		void run();
+
+	private:
+		GLFWwindow* window;
+		GLuint shaderProgram;
+		GLuint vertexShader;
+		GLuint fragmentShader;
+		GLuint VAO;
+		GLuint VBO;
+		int width, height;
+
+		
+};
---
+++ 5_semestr/ZPG/04-CV/Projekt.cpp
@@ -0,0 +1,13 @@
+#include "Application.h"
+
+int main(void)
+{
+	Application* app = new Application();
+	app->initialization(); //OpenGL inicialization
+
+	//Loading scene
+	app->createShaders();
+	app->createModels();
+	app->run(); //Rendering 
+	
+}
