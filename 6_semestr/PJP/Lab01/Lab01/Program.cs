aa091fe1d2ec929cdeb2f50ba6e704c4cca671d2
elsnoxx <83233897+elsnoxx@users.noreply.github.com>
2026-02-17 19:41:31 +0100

    lab 1 pjp init

---
+++ 6_semestr/PJP/Lab01/Lab01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int open = 0;
+            int close = 0;
+
+            input = input.Trim();
+            List<char> tokens = new List<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!char.IsWhiteSpace(input[i]) )
+                {
+
+                    tokens.Add(input[i]);
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                }
+            }
+            foreach (var item in tokens)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- 6_semestr/ZAO/README.md
+++ 6_semestr/ZAO/README.md
@@ -1,2 +1,3 @@
 # Základy analýzy obrazu (ZAO)
 
+Website: https://mrl.cs.vsb.cz//people/fusek/zao_course.html
