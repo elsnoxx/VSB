61341f5f1355891aa3be5ad7dc138ddc5d5a3aa8
elsnoxx <83233897+elsnoxx@users.noreply.github.com>
2026-03-05 19:35:42 +0100

    improvement of duck searching

--- 6_semestr/ZAO/lesson03/duckhunt.py
+++ 6_semestr/ZAO/lesson03/duckhunt.py
@@ -5,23 +5,29 @@ import pyautogui
 import time
 import os
 
-def loadFiles(path):
-    files = []
-    for file in os.listdir(path):
-        files.append(os.path.join(path, file))
-    return files
 
-def click_on_target(x, y):
+    
+
+def click_on_target(x, y, max_val):
     pyautogui.click(x, y, _pause=False)
-    print(f"Klikám... {x} a {y}")
+    print(f"Klikám... {x} a {y}, Maximální hodnota shody: {max_val:.2f}")
+
+red_files = []
+blask_files = []
+path = 'duckhunt/tragets'
+for file in os.listdir(path):
+    if(file.split('_')[0] == 'red' ):
+        red_files.append(os.path.join(path, file))
+    elif(file.split('_')[0] == 'black' ):
+        blask_files.append(os.path.join(path, file))
 
 
-files = loadFiles('duckhunt/tragets')
-print(files)
+print(red_files)
+print(blask_files)
 templates = []
-for file in files:
+for file in blask_files:
     template = cv.imread(file, 0)
-    template = cv.resize(template, (int(template.shape[1] * 2.5), int(template.shape[0] * 2.5)))
+    template = cv.resize(template, (int(template.shape[1] * 2.8), int(template.shape[0] * 2.8)))
     templates.append(template)
 
 
@@ -37,23 +43,27 @@ search_area = {"top": 0, "left": mon_width, "width": mon_width, "height": mon_he
 prev_x, prev_y = None, None
 cnt = 0
 
+avr = []
+
 with mss() as sct:
     while True:
         image = np.array(sct.grab(search_area))
         width, height = image.shape[1], image.shape[0]
-        image_gray = cv.cvtColor(np.array(image), cv.COLOR_RGB2GRAY)
+        image_gray = cv.cvtColor(np.array(image), cv.COLOR_BGRA2GRAY)
 
         for template in templates:
-            cnt += 1
+            # cnt += 1
+
+                
             res = cv.matchTemplate(image_gray, template, cv.TM_CCOEFF_NORMED)
             _, max_val, _, max_loc = cv.minMaxLoc(res)
 
 
-            print(f"Maximální hodnota shody: {max_val:.2f} na pozici {max_loc}.")
-            cv.rectangle(image_gray, max_loc, (max_loc[0] + template.shape[1], max_loc[1] + template.shape[0]), (0, 255, 0), 2)
-            cv.putText(image_gray, f"{max_val:.2f} {file}", (max_loc[0], max_loc[1] - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+            # print(f"Maximální hodnota shody: {max_val:.2f} na pozici {max_loc}.")
+            # cv.rectangle(image_gray, max_loc, (max_loc[0] + template.shape[1], max_loc[1] + template.shape[0]), (0, 255, 0), 2)
+            # cv.putText(image_gray, f"{max_val:.2f} {file}", (max_loc[0], max_loc[1] - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
-            if max_val >= 0.8:
+            if max_val >= 0.7:
                 # aktuální pozice středu objektu
                 x = max_loc[0] + template.shape[1] // 2 + mon_width
                 y = max_loc[1] + template.shape[0] // 2
@@ -64,7 +74,7 @@ with mss() as sct:
                     if dist > 100:   # nový target
                         print("Nový target - reset predikce")
                         prev_x, prev_y = None, None
-                        click_on_target(x, y)
+                        click_on_target(x, y, max_val)
 
                     else:
                         vx = x - prev_x
@@ -77,25 +87,26 @@ with mss() as sct:
 
                 else:
                     # první detekce → ještě nemáme rychlost
-                    click_on_target(x, y)
+                    click_on_target(x, y, max_val)
 
                 # uložíme aktuální pozici
                 prev_x, prev_y = x, y
 
                 # Zobrazení obrázku v OpenCV
                 
-
-        cv.imshow('Sablona', template)
-        image_gray = cv.resize(image_gray, (image_gray.shape[1] // 2, image_gray.shape[0] // 2))
-        cv.imshow('Snímek obrazovky', image_gray)
-        cv.waitKey(0)
-        cv.destroyAllWindows()
-        cnt = 0
+        avr.append(max_val)
+        # cv.imshow('Sablona', template)
+        # image_gray = cv.resize(image_gray, (image_gray.shape[1] // 2, image_gray.shape[0] // 2))
+        # cv.imshow('Snímek obrazovky', image_gray)
+        # cv.waitKey(0)
+        # cv.destroyAllWindows()
+        # cnt = 0
 
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
         
-        time.sleep(0.5)
+        # time.sleep(0.5)
+        # print(f"avr is {sum(avr) / len(avr):.2f}")
 
         # Zobrazení obrázku v OpenCV
         # cv.rectangle(image, max_loc, (max_loc[0] + template.shape[1], max_loc[1] + template.shape[0]), (0, 255, 0), 2)
