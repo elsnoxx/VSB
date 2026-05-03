3066d35fdeb926b150892e5f8415e71b02d1ed31
elsnoxx <ficek.risa@gmail.com>
2026-05-03 21:01:54 +0200

    Update Program.cs

--- 6_semestr/PJP/Projekt/Program.cs
+++ 6_semestr/PJP/Projekt/Program.cs
@@ -8,73 +8,119 @@ namespace Projekt
     internal class Program
     {
         static readonly string[] fileNames = ["PLC_t1.in.txt", "PLC_t2.in.txt", "PLC_t3.in.txt", "PLC_errors.in.txt"];
+        static string fileName = "";
+        static bool isVerbose = false;
         static void Main(string[] args)
         {
             Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
             //var fileName = "input.txt";
             //Console.WriteLine("Parsing: " + fileName);
 
-            //if (args.Length > 0)
+            //if (args.Length == 0)
             //{
-            //    fileName = args[0];
-            //    Console.WriteLine("Parsing: " + fileName);
-            //    CompilationProces(fileName);
-            //} else {
-            //    Console.WriteLine("No input file provided. Processing default test files...");
+            //    PrintHelp();
+            //    return;
             //}
 
+            // Kontrola, zda je přítomen argument -v
+            //isVerbose = args.Contains("-v");
+
+            // Získání cesty k souboru (vezmeme první argument, který není -v)
+            //string fileName = args.FirstOrDefault(a => a != "-v");
+
+            //if (string.IsNullOrEmpty(fileName))
+            //{
+            //    Console.WriteLine("Error: No input file specified.");
+            //    PrintHelp();
+            //    return;
+            //}
+
+            //if (File.Exists(fileName))
+            //{
+            //    CompilationProces(fileName, isVerbose);
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Error: File '{fileName}' not found.");
+            //}
+
+            // automatické zpracování všech testovacích souborů
             foreach (var file in fileNames)
             {
                 Console.WriteLine($"\n--- Processing {file} ---");
-                CompilationProces(file);
+                CompilationProces(file, isVerbose);
             }
 
         }
 
-        public static void CompilationProces(string fileName)
+        public static void PrintHelp()
+        {
+            Console.WriteLine("Usage: dotnet run <input_file> [-v]");
+            Console.WriteLine("Options:");
+            Console.WriteLine("  -v    Verbose mode (prints tokens, parse tree and generated code)");
+        }
+
+        public static void CompilationProces(string fileName, bool verbose)
         {
             var input = File.ReadAllText(fileName);
             var inputStream = new AntlrInputStream(input);
             var lexer = new PLCProjectLexer(inputStream);
             var tokenStream = new CommonTokenStream(lexer);
-            var parser = new PLCProjectParser(tokenStream);
 
-            // 1. Syntaktická analýza
+            // --- VERBOSE: VÝPIS TOKENŮ ---
+            if (verbose)
+            {
+                Console.WriteLine("\n--- TOKENS ---");
+                tokenStream.Fill();
+                foreach (var token in tokenStream.GetTokens())
+                {
+                    Console.WriteLine(token.ToString());
+                }
+                tokenStream.Reset();
+            }
+
+            var parser = new PLCProjectParser(tokenStream);
             var errorListener = new VerboseListener();
             parser.RemoveErrorListeners();
             parser.AddErrorListener(errorListener);
 
             var tree = parser.prog();
 
+            // --- VERBOSE: VÝPIS STROMU ---
+            if (verbose)
+            {
+                Console.WriteLine("\n--- PARSE TREE ---");
+                Console.WriteLine(tree.ToStringTree(parser));
+            }
+
             if (errorListener.HadError)
             {
-                Console.WriteLine("Computation stopped due to syntax errors.");
+                Console.WriteLine("\nComputation stopped due to syntax errors.");
                 return;
             }
 
-            // 2. Kontrola typů
             var typeChecker = new TypeCheckingVisitor();
             typeChecker.Visit(tree);
 
             if (typeChecker.Errors.Any())
             {
                 foreach (var err in typeChecker.Errors) Console.WriteLine(err);
-                Console.WriteLine("Computation stopped due to type errors.");
+                Console.WriteLine("\nComputation stopped due to type errors.");
                 return;
             }
 
-            // 3. Generování kódu
-            Console.WriteLine("Type checking passed! Generating code...");
-
             var codeGen = new CodeGeneratorVisitor(typeChecker);
             string finalCode = codeGen.Visit(tree);
-
-            // Vyčištění prázdných řádků a výpis
             finalCode = string.Join("\n", finalCode.Split('\n', StringSplitOptions.RemoveEmptyEntries));
-            Console.WriteLine("--- GENERATED CODE ---");
-            //Console.WriteLine(finalCode);
 
-            // Volitelně uložení do souboru
+            // --- VERBOSE: VÝPIS GENEROVANÉHO KÓDU ---
+            if (verbose)
+            {
+                Console.WriteLine("\n--- GENERATED CODE ---");
+                Console.WriteLine(finalCode);
+                Console.WriteLine("----------------------\n");
+            }
+
             File.WriteAllText(fileName + ".out", finalCode);
 
             try
